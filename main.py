d7c88093ca060fd2cfb7e8ace7a0efd2740fb06b
vladiik124 <vladimirkotliar8@gmail.com>
2025-03-03 21:45:31 +0200

    Made mouse movement

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time #sleep
 import keyboard #detecting key presses
 
 #title
-print("BOKKI THE ROCK AUTO NIJIKA FOR BROKIES\npress enter to start")
+print("BOKKI THE ROCK AUTO NIJIKA FOR BROKIES\nSpam F to stop\npress enter to start")
 input()
 
 def is_process_running(process_name):
@@ -18,12 +18,26 @@ def is_process_running(process_name):
 def get_screen_info():
     return GetSystemMetrics(0),GetSystemMetrics(1)
 
+def moveTo(NewX, NewY, step):
+    x, y = pydirectinput.position()
+
+    stepX = (NewX - x) / step
+    stepY = (NewY - y) / step
+
+    for i in range(step):
+        x += stepX
+        y += stepY
+        pydirectinput.moveTo(int(x), int(y))
+        time.sleep(0.001)  # Smooth movement delay
+
+    pydirectinput.moveTo(NewX, NewY)  # Ensure final position is accurate
+
 
 #Check if roblox process is open
 if (is_process_running("RobloxPlayerBeta.exe")):
-        #Check screen resolution
+        #Check screen resolutionf
         print(f"screen size {get_screen_info()}")
-        #ask when player is sure
+        #ask when player is sur
 
 
         #depending on screen res, automate.
@@ -38,21 +52,16 @@ if (is_process_running("RobloxPlayerBeta.exe")):
         time.sleep(1)
         print(1)
         time.sleep(1)
-        pydirectinput.moveTo(int(width / 2), int(height / 2), duration=4)
+        moveTo(int(width / 2), int(height / 2), 10)
         time.sleep(1)
         print("skid")
 
         x=True
-        while(x):
-            if (keyboard.is_pressed('f')):
-                x=False
-            pydirectinput.moveTo(int(width/2), int(height-200))
+        while(not keyboard.is_pressed('f')):
+            moveTo(int(width/2), int(height-200),1)
             pydirectinput.click()
-            pydirectinput.moveTo(int(width/2), int(height/2+350))
+            moveTo(int(width/2), int(height/2+250), 1)
             pydirectinput.click()
 
 else:
      print("roblox not open! try again")
-
-
-
